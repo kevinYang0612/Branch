0fac861eae95b164b0916bdd47aac7fbbed9a8ef
Kevin Yang <91995544+kevinYang0612@users.noreply.github.com>
2023-05-29 23:03:35 -0700

    Update template.js

--- template.js
+++ template.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) =>
 {
